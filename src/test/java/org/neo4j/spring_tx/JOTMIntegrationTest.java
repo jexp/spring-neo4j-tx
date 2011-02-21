e86788ac15b8896682c38e8a7ee90040deaed8ac
Michael Hunger <github@jexp.de>
2011-02-21 13:43:34 +0100

    added lucene index dependency

--- src/test/java/org/neo4j/spring_tx/JOTMIntegrationTest.java
+++ src/test/java/org/neo4j/spring_tx/JOTMIntegrationTest.java
@@ -2,10 +2,12 @@ package org.neo4j.spring_tx;
 
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.kernel.Config;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.objectweb.jotm.Current;
-import org.objectweb.jotm.TransactionImpl;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.transaction.jta.ManagedTransactionAdapter;
@@ -13,8 +15,6 @@ import org.springframework.transaction.jta.ManagedTransactionAdapter;
 import javax.transaction.NotSupportedException;
 import javax.transaction.SystemException;
 import javax.transaction.Transaction;
-import javax.transaction.TransactionManager;
-
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -33,8 +33,18 @@ public class JOTMIntegrationTest {
         JtaTransactionManager tm = ctx.getBean("transactionManager", JtaTransactionManager.class);
         Transaction transaction = tm.createTransaction("jotm", 1000);
         assertEquals(ManagedTransactionAdapter.class, transaction.getClass());
-        assertEquals(Current.class, ((ManagedTransactionAdapter)transaction).getTransactionManager().getClass());
-        Map<Object,Object> config = ((EmbeddedGraphDatabase) gds).getConfig().getParams();
-        assertEquals("spring-jta",config.get(Config.TXMANAGER_IMPLEMENTATION));
+        assertEquals(Current.class, ((ManagedTransactionAdapter) transaction).getTransactionManager().getClass());
+        Map<Object, Object> config = ((EmbeddedGraphDatabase) gds).getConfig().getParams();
+        assertEquals("spring-jta", config.get(Config.TXMANAGER_IMPLEMENTATION));
+
+        org.neo4j.graphdb.Transaction tx = gds.beginTx();
+        try {
+            Index<Node> index = gds.index().forNodes("node");
+            IndexHits<Node> indexHits = index.get("field", "value");
+            assertEquals(false, indexHits.hasNext());
+            tx.success();
+        } finally {
+            tx.finish();
+        }
     }
 }
